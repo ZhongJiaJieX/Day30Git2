e8fbd9f4ad682fedb93476dade1f82fb6a2f44ca
ZhongJiaJieX <zhongjiajiex@live.com>
2016-10-23 16:29:19 +0800

    添加打印

--- app/src/main/java/com/zjj/test/day30git/MainActivity.java
+++ app/src/main/java/com/zjj/test/day30git/MainActivity.java
@@ -2,12 +2,17 @@ package com.zjj.test.day30git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "print";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e(TAG, "onCreate: " );
     }
 }
